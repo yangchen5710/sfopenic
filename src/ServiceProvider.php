2cb8ea27ece6352bde8dcec0cbbb58c411b00624
yangchen <547098838@qq.com>
2021-07-23 14:39:28 +0800

    test

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,11 +5,26 @@ namespace Ycstar\Sfopenic;
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     protected $defer = true;
+    
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/config.php');
+
+        $this->publishes([$source => config_path('sfopenic.php')], 'ycstar-sfopenic');
+       
+        $this->mergeConfigFrom($source, 'sfopenic');
+    }
+
 
     public function register()
-    {
+    {   
+        $this->setupConfig();
+
         $this->app->singleton(Sfopenic::class, function(){
-            return new Sfopenic(config('services.sfopenic.host'),config('services.sfopenic.dev_id'),config('services.sfopenic.dev_key'));
+            return new Sfopenic(config('sfopenic.host'),config('sfopenic.dev_id'),config('sfopenic.dev_key'));
         });
 
         $this->app->alias(Sfopenic::class, 'sfopenic');
---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+  return [
+  	
+    'host' => env('SF_OPENIC_HOST'),
+
+    'dev_id' => env('SF_OPENIC_DEV_ID'),
+
+    'dev_key' => env('SF_OPENIC_DEV_KEY')
+
+  ];
